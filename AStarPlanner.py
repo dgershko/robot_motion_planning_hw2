ade2e8a08bdb2a9026ab0c09fa7a22fd85956616
dgershko <dgershko@gmail.com>
2024-02-24 23:17:55 +0200

    added astar, most of rrt (non star version)

--- AStarPlanner.py
+++ AStarPlanner.py
@@ -1,8 +1,12 @@
 import numpy as np
+import heapq
+from MapEnvironment import MapEnvironment
 
 class AStarPlanner(object):    
-    def __init__(self, planning_env):
-        self.planning_env = planning_env
+    def __init__(self, planning_env, epsilon=1):
+        self.planning_env = planning_env # type: MapEnvironment
+        # self.epsilon = epsilon
+        self.epsilon = 20
 
         # used for visualizing the expanded nodes
         # make sure that this structure will contain a list of positions (states, numpy arrays) without duplicates
@@ -12,18 +16,70 @@ class AStarPlanner(object):
         '''
         Compute and return the plan. The function should return a numpy array containing the states (positions) of the robot.
         '''
-
-        # initialize an empty plan.
         plan = []
+        start_position = tuple(self.planning_env.start)
+        goal_position = tuple(self.planning_env.goal)
+        frontier = Frontier()
+        came_from = {}
+        g_score = {start_position: 0}
+        f_score = {start_position: self.heuristic(start_position)}
+        frontier.insert(start_position, f_score[start_position])
 
-        # TODO: Task 4.3
-
-        return np.array(plan)
+        while frontier:
+            current = frontier.pop()
+            if current == goal_position:
+                while current != start_position:
+                    plan.insert(0, current)
+                    current = came_from[current]
+                plan.insert(0, current)
+                return np.array(plan)
+            
+            self.expanded_nodes.append(current)
+            for neighbor in self.get_neighbors(current):
+                tentative_score = g_score[current] + np.linalg.norm(np.array(current) - np.array(neighbor))
+                if tentative_score < g_score.get(neighbor, np.inf):
+                    came_from[neighbor] = current
+                    g_score[neighbor] = tentative_score
+                    f_score[neighbor] = tentative_score + self.epsilon * self.heuristic(neighbor)
+                    if neighbor not in frontier:
+                        frontier.insert(neighbor, f_score[neighbor])
+        return np.array([])
 
     def get_expanded_nodes(self):
         '''
         Return list of expanded nodes without duplicates.
         '''
-
-        # used for visualizing the expanded nodes
+        self.expanded_nodes = list(set(self.expanded_nodes))
         return self.expanded_nodes
+
+    def heuristic(self, pos):
+        return np.linalg.norm(pos - self.planning_env.goal)
+
+    def get_neighbors(self, pos):
+        x, y = pos
+        deltas = [-1, 0, 1]
+        neighbors = [(x + dx, y + dy) for dx in deltas for dy in deltas]
+        neighbors.remove(pos)
+        neighbors = [neighbor for neighbor in neighbors if self.planning_env.state_validity_checker(neighbor)]
+        return neighbors
+
+
+class Frontier:
+    def __init__(self):
+        self.pq = []
+        self.set = set()
+    
+    def pop(self):
+        _, element = heapq.heappop(self.pq)
+        self.set.remove(element)
+        return element
+
+    def insert(self, element, priority):
+        heapq.heappush(self.pq, (priority, element))
+        self.set.add(element)
+
+    def __contains__(self, element):
+        return element in self.set
+
+    def __bool__(self):
+        return bool(self.set)
--- RRTPlanner.py
+++ RRTPlanner.py
@@ -1,5 +1,7 @@
 import numpy as np
 from RRTTree import RRTTree
+from MapEnvironment import MapEnvironment
+from shapely.geometry import Point, LineString, Polygon
 import time
 
 class RRTPlanner(object):
@@ -7,12 +9,14 @@ class RRTPlanner(object):
     def __init__(self, planning_env, ext_mode, goal_prob):
 
         # set environment and search tree
-        self.planning_env = planning_env
+        self.planning_env = planning_env # type: MapEnvironment
         self.tree = RRTTree(self.planning_env)
+        self.tree.add_vertex(self.planning_env.start)
 
         # set search params
         self.ext_mode = ext_mode
         self.goal_prob = goal_prob
+        self.step_size = 1.5
 
     def plan(self):
         '''
@@ -22,8 +26,15 @@ class RRTPlanner(object):
 
         # initialize an empty plan.
         plan = []
+        while not self.tree.is_goal_exists(self.planning_env.goal):
+            rand_state = self.get_random_sample()
+            near_idx, near_state = self.tree.get_nearest_state(rand_state)
+            new_state = self.extend(near_state, rand_state)
+            if self.planning_env.edge_validity_checker(near_state, new_state):
+                new_idx = self.tree.add_vertex(new_state)
+                self.tree.add_edge(near_idx, new_idx, np.linalg.norm(np.array(near_state) - np.array(new_state)))
+        
 
-        # TODO: Task 4.4
         
         # print total path cost and time
         print('Total cost of path: {:.2f}'.format(self.compute_cost(plan)))
@@ -36,9 +47,7 @@ class RRTPlanner(object):
         Compute and return the plan cost, which is the sum of the distances between steps.
         @param plan A given plan for the robot.
         '''
-        # TODO: Task 4.4
-
-        pass
+        return LineString(plan).length
 
     def extend(self, near_state, rand_state):
         '''
@@ -46,6 +55,23 @@ class RRTPlanner(object):
         @param near_state The nearest position to the sampled position.
         @param rand_state The sampled position.
         '''
-        # TODO: Task 4.4
+        if self.ext_mode == 'E1':
+            return rand_state
+        elif self.ext_mode == 'E2':
+            direction = rand_state - near_state
+            distance = np.linalg.norm(direction)
+            if distance <= self.step_size:
+                return rand_state
+            unit_direction_vector = direction / distance
+            return near_state + unit_direction_vector * self.step_size
 
-        pass
+
+    def get_random_sample(self):
+        if np.random.uniform(0, 1) <= self.goal_prob:
+            return self.planning_env.goal
+        x = np.random.uniform(*self.planning_env.xlimit)
+        y = np.random.uniform(*self.planning_env.ylimit)
+        while not self.planning_env.state_validity_checker((x, y)):
+            x = np.random.uniform(*self.planning_env.xlimit)
+            y = np.random.uniform(*self.planning_env.ylimit)
+        return (x, y)
